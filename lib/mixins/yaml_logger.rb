3a745348db62ab860f888d0ced1f75d925de7733
jasonguyperson <jasonshane87@gmail.com>
2024-09-14 15:34:43 -0600

    wip

--- lib/mixins/yaml_logger.rb
+++ lib/mixins/yaml_logger.rb
@@ -1,23 +1,18 @@
 module YamlLogger
   def self.write(activity:, data:)
     File.open(file_path, "a") do |file|
-      file.puts formatted_entry.to_yaml
-    end
-
-    file_path = "log/#{today}.yml"
-    File.open(file_path, 'w') do |file|
-      file.write(YAML.dump(data))
+      file.puts formatted_entry(activity:, data:).to_yaml
     end
   end
 
   private
 
-  def formatted_entry
-    {
-      timestamp => {
-        activity: activity,
-      }
-    }
+  def file_path
+    "log/#{today}.yml"
+  end
+
+  def formatted_entry(activity)
+    { timestamp => { activity.to_sym => data } }
   end
 
   def today
