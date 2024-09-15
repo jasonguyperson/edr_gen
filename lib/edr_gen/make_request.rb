75b8c74f8181e8dd5029af85074e2ceefee8f078
jasonguyperson <jasonshane87@gmail.com>
2024-09-15 16:43:49 -0600

    simple socket transmit and receive

--- lib/edr_gen/make_request.rb
+++ lib/edr_gen/make_request.rb
@@ -3,21 +3,22 @@
 require 'json'
 require 'socket'
 
-# Used to make requests to external APIs
+# Used to transmit data to a server via a TCP socket connection.
 class MakeRequest < EdrGenBase
   ACTIVITY = 'transmit'
+  TIMEOUT = 5
 
   def initialize(args)
     super(args)
 
-    @server_host  = args[0] || 'socat.org'
-    @server_port  = args[1] || 7
+    @server_host  = args[0] || 'tcpbin.com'
+    @server_port  = args[1] || 4242
     @data_to_send = args[2] || 'placeholder data'
     @process_info = ProcTable.ps(pid: Process.pid)
   end
 
   def call
-    puts "  Initiating request..."
+    puts "  Initiating TCP connection..."
     connect_and_transmit
     write_log_entry
   end
@@ -27,16 +28,18 @@ class MakeRequest < EdrGenBase
   attr_reader :server_host, :server_port, :data_to_send, :process_info, :bytes_sent, :source, :destination
 
   def connect_and_transmit
-    socket = TCPSocket.new(server_host, server_port)
+    Socket.tcp(server_host, server_port, connect_timeout: TIMEOUT) do |socket|
+      @source = "#{socket.local_address.ip_address}:#{socket.local_address.ip_port}"
+      @destination = "#{socket.remote_address.ip_address}:#{socket.remote_address.ip_port}"
 
-    @source = "#{socket.local_address.ip_address}:#{socket.local_address.ip_port}"
-    @destination = "#{socket.remote_address.ip_address}:#{socket.remote_address.ip_port}"
-    @bytes_sent = socket.write(data_to_send)
-    puts Rainbow("  #{socket.read}").color(:green)
+      @bytes_sent = socket.write(data_to_send)
+      socket.puts
+      response = socket.gets
 
-    socket.close
+      puts Rainbow("  Response: #{response}").green
+    end
   rescue StandardError => e
-    abort(Rainbow("  An error occurred: #{e.message}").color(:red))
+    abort(Rainbow("  An error occurred: #{e.class}: #{e.message}").red)
   end
 
   def log_data
